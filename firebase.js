6e48899546382dfcb2f562e0eb70f43ae2bdf9dc
Ramya <ramyasri.thokala2727@gmail.com>
2026-01-14 19:07:44 +0530

    Test Firebase console log

--- firebase.js
+++ firebase.js
@@ -14,3 +14,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+console.log("🔥 Firebase connected successfully");
